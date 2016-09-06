58b25c03cb3ba79e8af4ba965297bd6f3c07455c
alconaut <alconautgh@yandex.com>
2016-09-06 13:53:59 +0600

    Implemented a test JS object for internal use

---
+++ internal/test/object.go
@@ -0,0 +1,54 @@
+package test
+
+import (
+	"github.com/colegion/jq/internal"
+)
+
+// JS implements an Object interface and is used for
+// testing on non-JS platforms.
+// Calls to its methods are recorded and can be
+// retrieved using Test() method.
+type JS struct {
+	calls []Call
+}
+
+// Call is a type that represents information about a
+// method call.
+type Call struct {
+	method string
+	args   []interface{}
+}
+
+// Get returns an Object containing information about
+// all the previous calls and the current one.
+func (o *JS) Get(key string) internal.Object {
+	o.calls = append(o.calls, Call{"Get", []interface{}{key}})
+	return o
+}
+
+// Call returns an Object containing information about
+// all the previous calls and the current one.
+func (o *JS) Call(name string, args []interface{}) internal.Object {
+	o.calls = append(o.calls, Call{"Call", []interface{}{name, args}})
+	return o
+}
+
+// Interface returns an Object containing information about
+// all the previous calls and the current one.
+func (o *JS) Interface() interface{} {
+	o.calls = append(o.calls, Call{"Interface", []interface{}{}})
+	return o
+}
+
+// New returns an Object containing information about
+// all the previous calls and the current one.
+func (o *JS) New(args []interface{}) internal.Object {
+	o.calls = append(o.calls, Call{"New", []interface{}{args}})
+	return o
+}
+
+// Test returns information about all the calls of the object
+// that have ever been made.
+func (o *JS) Test() interface{} {
+	return o.calls
+}
---
+++ internal/test/object_test.go
@@ -0,0 +1,46 @@
+package test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/colegion/jq/internal"
+)
+
+const incorrectCalls = "Incorrect list of calls. Expected %#v, got %#v."
+
+func TestJS_NoCalls(t *testing.T) {
+	o := Global
+	var exp []Call
+	if cs := o.Test(); !reflect.DeepEqual(cs.([]Call), exp) {
+		t.Errorf(incorrectCalls, exp, cs)
+	}
+}
+
+func TestJS(t *testing.T) {
+	arg1 := "xxx"
+	o := Global.Get(arg1)
+
+	arg2 := "eval"
+	arg3 := "x1"
+	arg4 := "x2"
+	o = o.Call(arg2, []interface{}{arg3, arg4})
+
+	arg5 := "a1"
+	arg6 := "a2"
+	o = o.New([]interface{}{arg5, arg6})
+
+	o = o.Interface().(internal.Object)
+
+	exp := []Call{
+		Call{"Get", []interface{}{arg1}},
+		Call{"Call", []interface{}{arg2, []interface{}{arg3, arg4}}},
+		Call{"New", []interface{}{[]interface{}{arg5, arg6}}},
+		Call{"Interface", []interface{}{}},
+	}
+	if cs := o.Test(); !reflect.DeepEqual(cs, exp) {
+		t.Errorf(incorrectCalls, exp, cs)
+	}
+}
+
+var Global = &JS{}
